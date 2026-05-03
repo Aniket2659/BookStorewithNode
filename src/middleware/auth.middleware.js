46435d00a641535993e56e31aea2bdc94e9a6c22
Aniket Babar <aniketbabar@Anikets-MacBook-Air.local>
2026-05-03 17:02:59 +0530

    added authenticate middleware to authorize user

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+export const authenticate = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({
+        success: false,
+        message: "No token provided",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
+};
